8327dfab9857ebe7f7f891cc655e1a592e0f1544
Raunak Redkar <raunak.redkar@gmail.com>
2024-04-26 20:00:40 +0200

    adding some competitive programming stuff

---
+++ cp/template.cpp
@@ -0,0 +1,65 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+         
+using namespace std;
+using namespace __gnu_pbds;
+ 
+/* typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set; */
+         
+#define el "\n"
+#define faster ios_base::sync_with_stdio(0); cin.tie(0);
+#define sq(x) (x)*(x)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+         
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+         
+#define all(x) (x).begin(), (x).end()
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (b-1); i >= (a); --i)
+#define REP(i, a, b) for (int i = (a); i <= (b); ++i)
+#define trav(a, x) for (auto &a : x)
+         
+struct chash { // large odd number for C
+      const uint64_t C = ll(4e18 * acos(0)) | 71;
+      ll operator()(ll x) const { return __builtin_bswap64(x * C); }
+};
+ 
+template<typename T> using fast_set = __gnu_pbds::gp_hash_table<T, null_type, chash>;
+template<typename T, typename H> using fast_set_h = __gnu_pbds::gp_hash_table<T, null_type, H>;
+template<typename T, typename U> using fast_map = __gnu_pbds::gp_hash_table<T, U, chash>;
+ 
+#ifdef DEBUG
+#include "/home/ac41991/kod/cp/lib/debug.cpp"
+#else
+#define dbg(...)
+#define dbgArr(...)
+#endif
+
+const int inf = 1e9;
+
+void solve() {
+
+}
+
+int main() { 
+	faster
+        int test = 1; // cin >> test;
+	REP(tc, 1, test) {
+		solve();
+	}
+}
